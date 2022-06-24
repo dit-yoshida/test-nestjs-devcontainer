78ef3fe1818254202f1dd03169f15a978d17feec
Tsuyoshi Yoshida <yoshida.tsuyoshi@ditgroup.jp>
2022-06-24 00:31:09 +0000

    feat: Prisma 導入とDB接続

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostService } from './services/prisma/post.service';
+import { PrismaService } from './services/prisma/prisma.service';
+import { UserService } from './services/prisma/user.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserService, PostService, PrismaService],
 })
 export class AppModule {}
